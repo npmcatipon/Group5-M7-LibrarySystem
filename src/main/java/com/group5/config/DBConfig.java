10a6092e581c35b24ac2b983b4b2a6b7926c6bff
npmcatipon <npmcatipon@bpi.com.ph>
2026-02-18 12:16:23 +0800

    deleted old db config and dbutil

--- src/main/java/com/group5/config/DBConfig.java
+++
@@ -1,8 +0,0 @@
-package com.group5.config;
-public class DBConfig {
-
-	public static final String URL = "jdbc:postgresql://localhost:5432/group5";
-	public static final String USERNAME = "postgres";
-	public static final String PASSWORD = "postgres";
-	
-}
